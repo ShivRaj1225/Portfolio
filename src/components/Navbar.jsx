e4829b501c675c25bd0842944244c6fd4a4e225e
Shiv <shivrajdesai222@gmail.com>
2025-07-26 23:14:36 +0530

    final8

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
               spy={true}
               smooth={true}
               duration={700}
-              offset={-15}
+              offset={-16}
               className="hover:text-[#9929EA] cursor-pointer">
               About
             </ScrollLink>
@@ -141,7 +141,7 @@ const Navbar = () => {
                 to="contact"
                 smooth={true}
                 duration={700}
-                offset={-10}
+                offset={-5}
                 onClick={closeMenu}
                 className="block px-6 py-4 hover:text-[#9929EA] cursor-pointer">
                 Contact
